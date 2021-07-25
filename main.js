38679a692a0327f7342e4486a912fc48157bfe2d
benten <orthrhope333@gmail.com>
2021-07-25 18:20:07 +0900

    セクション追加

---
+++ main.js
@@ -0,0 +1,16 @@
+const pro1 = document.getElementById("pro1")
+const pro2 = document.getElementById("pro2")
+const pro3 = document.getElementById("pro3")
+
+
+pro1.onmouseover = function() {
+  pro1.classList.toggle("rounded")
+}
+
+pro2.onmouseover = function() {
+    pro2.classList.toggle("rounded")
+  }
+
+  pro3.onmouseover = function() {
+    pro3.classList.toggle("rounded")
+  }
